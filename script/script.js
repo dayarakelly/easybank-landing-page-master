a315a3dc4d5c0650d2f39c305735092ec574f5e7
Dayara Kelly <dayarakellyalmeida@gmail.com>
2023-01-02 16:04:10 -0300

    Alteração no icone do menu hamburguer

--- script/script.js
+++ script/script.js
@@ -2,21 +2,6 @@ let menu_sandwich = document.querySelector('#menu_sandwich')
 let menu = document.querySelector('.menu')
 let shadow = document.querySelector('#shadow')
 let close = document.querySelector('.menu_fechar')
-let menu_items =[...document.querySelectorAll('.link_nav')]
-
-menu_items[0].addEventListener('click', rolagem_menu_responsivo) 
-menu_items[1].addEventListener('click', rolagem_menu_responsivo) 
-menu_items[2].addEventListener('click', rolagem_menu_responsivo) 
-menu_items[3].addEventListener('click', rolagem_menu_responsivo) 
-menu_items[4].addEventListener('click', rolagem_menu_responsivo) 
-
-function rolagem_menu_responsivo(){
-    menu.classList.remove('move_menu')
-    menu_sandwich.classList.remove('menu_fechar')
-    menu_sandwich.classList.add('menu_abrir')
-    shadow.classList.remove('shadow')
-    shadow.classList.remove('move_shadow')
-}
 
 menu_sandwich.addEventListener('click', function () {
     menu.classList.toggle('move_menu')
